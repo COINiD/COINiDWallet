59ae74b260089ddff7626a7819e94d856c8c27fd
rikardwissing <rikard.wissing@teamtailor.com>
2019-03-22 15:48:05 +0100

    Ignore not import warnings

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket',
+  'Sending `didUpdateState`',
+  'Warning: Async Storage',
+  'Remote debugger is in a background',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
