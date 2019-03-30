323ad10194782b6908c2497a4aa6afefba3e26a5
rikardwissing <rikard.wissing@teamtailor.com>
2019-03-30 15:01:52 +0100

    Bump release and some improvements to about screen

--- src/settingstree/About.js
+++ src/settingstree/About.js
@@ -12,8 +12,30 @@ const openCommit = (commit) => {
   Linking.openURL(`https://github.com/COINiD/COINiDWallet/commit/${commit}`);
 };
 
-const openRelease = (release) => {
-  Linking.openURL(`https://github.com/COINiD/COINiDWallet/release/${release}`);
+const openReleaseTag = (tag) => {
+  Linking.openURL(`https://github.com/COINiD/COINiDWallet/releases/tag/${tag}`);
+};
+
+const getLinks = () => {
+  const items = [];
+
+  if (build.commit) {
+    items.push({
+      title: 'View commit on GitHub',
+      onPress: () => openCommit(build.commit),
+      hideChevron: false,
+    });
+  }
+
+  if (build.tag) {
+    items.push({
+      title: 'View release on GitHub',
+      onPress: () => openReleaseTag(build.tag),
+      hideChevron: false,
+    });
+  }
+
+  return items;
 };
 
 const getBlockheight = ({ activeWallets }) => {
@@ -51,7 +73,7 @@ const About = state => [
     items: [
       {
         title: 'Version',
-        rightTitle: build.version,
+        rightTitle: build.tag ? build.tag : `untagged-v${build.version}`,
         hideChevron: true,
       },
       {
@@ -74,18 +96,7 @@ const About = state => [
     ],
   },
   {
-    items: [
-      {
-        title: 'View commit on GitHub',
-        onPress: () => openCommit(build.commit),
-        hideChevron: false,
-      },
-      {
-        title: 'View release notes',
-        onPress: () => openRelease(build.version),
-        hideChevron: false,
-      },
-    ],
+    items: getLinks(),
   },
 ];
 
